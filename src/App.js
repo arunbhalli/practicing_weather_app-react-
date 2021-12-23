8d5a0761e34554c9352fd23840c645db9a9a0b76
arunbhalli <tomarbhalli@email.com>
2021-12-23 11:33:46 +0100

    adds date foe showing in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react'
+/* import React, { useState, useEffect } from 'react'; */
+import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
+
 import './App.css';
 const App = () => {
+  const weatherdata = (d) => {
+    let months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    let newDate = new Date()
+    let date = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let year = newDate.getFullYear();
+    return `${year}  ${month}  ${date}`
+    
+  };
+
   return (
     <div>
-      Welcome to my projects
+      <div>
+        <DeviceThermostatIcon />
+        <div>{weatherdata(new Date())}</div>
+      </div>
+      StockHolme
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
