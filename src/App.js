d004c5eb077ea4ef6909c8b383812d3f1b3163f6
arunbhalli <tomarbhalli@email.com>
2021-12-27 08:00:58 +0100

    adds try catch shows temperature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-/* import React, { useState, useEffect } from 'react'; */
+import React, { useState, useEffect } from 'react';
 import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
 
 import './App.css';
+import axios from 'axios';
 const App = () => {
-  const weatherdata = (d) => {
-    let months = [
+   const weatherdata = (d) => {
+      let months = [
       'January',
       'February',
       'March',
@@ -19,14 +20,23 @@ const App = () => {
       'December',
     ];
     let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-    let newDate = new Date()
     let date = days[d.getDay()];
     let month = months[d.getMonth()];
-    let year = newDate.getFullYear();
-    return `${year}  ${month}  ${date}`
-    
+    let year = d.getFullYear();
+    return `${year}  ${month}  ${date}`;
   };
+  const [temperature,setTemperature] =useState(null)
+ const  getData =async()=> {
+   try {const response = await axios.get("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=019427518edfd5cee2c02743f6571077");
+   setTemperature(response.data.main.temp)
+    console.log(response.data)
+  }catch (e) {
+    console.error(e)
+  }  
+  }
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div>
@@ -34,7 +44,7 @@ const App = () => {
         <DeviceThermostatIcon />
         <div>{weatherdata(new Date())}</div>
       </div>
-      StockHolme
+      <h1>{temperature}</h1>
     </div>
   );
 };
